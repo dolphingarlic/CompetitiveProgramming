62cc101c50a601dad1142ee41f97c7219481d72a
Andi Qu <dolphingarlic@gmail.com>
2019-08-25 14:46:23 +0200

    Added 2003 Maintain and 2007 Sails

---
+++ IOI/IOI03_maintain.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#pragma GCC Optimize("O3")
+#define FOR(i, x, y) for (int i = x; i < y; i++)
+#define MOD 1000000007
+typedef long long ll;
+using namespace std;
+
+set<pair<int, pair<int, int>>> s;
+int component[201];
+
+int fund(int a) {
+    while (a != component[a]) component[a] = component[component[a]], a = component[a];
+    return a;
+}
+int onion(int a, int b) { component[fund(a)] = component[fund(b)]; }
+
+int main() {
+    iostream::sync_with_stdio(false);
+    cin.tie(0);
+    int n, w;
+    cin >> n >> w;
+
+    FOR(i, 0, w) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        s.insert({c, {a, b}});
+
+        FOR(j, 1, n + 1) component[j] = j;
+
+        int ans = 0, cnt = 0;
+        for (pair<int, pair<int, int>> i : s) {
+            if (fund(i.second.first) != fund(i.second.second)) {
+                cnt++, ans += i.first;
+                onion(i.second.first, i.second.second);
+            }
+        }
+
+        if (cnt != n - 1) cout << -1 << endl;
+        else cout << ans << endl;
+    }
+    return 0;
+}
---
+++ IOI/IOI07_sails.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#pragma GCC Optimize("O3")
+#define FOR(i, x, y) for (ll i = x; i < y; i++)
+typedef long long ll;
+using namespace std;
+
+ll MAXH;
+ll bit1[100001], bit2[100001];
+pair<ll, ll> a[100001];
+
+void update(ll l, ll r, ll val) {
+    for (ll i = l; i <= MAXH; i += (i & (-i))) bit1[i] += val;
+    for (ll i = r + 1; i <= MAXH; i += (i & (-i))) bit1[i] -= val;
+
+    for (ll i = l; i <= MAXH; i += (i & (-i))) bit2[i] += val * (l - 1);
+    for (ll i = r + 1; i <= MAXH; i += (i & (-i)))  bit2[i] -= val * r;
+}
+ll query(ll l, ll r) {
+    ll ans = 0;
+    for (ll i = r; i; i -= (i & (-i))) ans += bit1[i] * r;
+    for (ll i = l - 1; i; i -= (i & (-i))) ans -= bit1[i] * (l - 1);
+
+    for (ll i = r; i; i -= (i & (-i))) ans -= bit2[i];
+    for (ll i = l - 1; i; i -= (i & (-i))) ans += bit2[i];
+    return ans;
+}
+
+int main() {
+    iostream::sync_with_stdio(false);
+    cin.tie(0);
+    ll n;
+    cin >> n;
+    FOR(i, 0, n) cin >> a[i].first >> a[i].second;
+    sort(a, a + n);
+    MAXH = a[n - 1].first;
+
+    ll ans = 0;
+    FOR(i, 0, n) {
+        ll ub = query(a[i].first - a[i].second + 1, a[i].first - a[i].second + 1);
+
+        ll l = a[i].first - a[i].second + 1, r = a[i].first + 1;
+        while (l != r) {
+            ll mid = (l + r) / 2;
+            if (query(mid, mid) < ub) r = mid;
+            else l = mid + 1;
+        }
+
+        ll pos = l;
+        ans += query(pos, a[i].first);
+        update(pos, a[i].first, 1);
+
+        l = 1, r = a[i].first;
+        while (l != r) {
+            ll mid = (l + r) / 2;
+            if (query(mid, mid) > ub) l = mid + 1;
+            else r = mid;
+        }
+
+        ans += query(l, l + a[i].second - a[i].first + pos - 2);
+        update(l, l + a[i].second - a[i].first + pos - 2, 1);
+    }
+
+    cout << ans;
+    return 0;
+}
