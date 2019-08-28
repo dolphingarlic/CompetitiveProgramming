6149533d1bc2346f16cca240335b512da4ac8924
Andi Qu <dolphingarlic@gmail.com>
2019-08-28 15:57:28 +0200

    Made comments better

--- APIO/APIO17_koala.cpp
+++ APIO/APIO17_koala.cpp
@@ -1,7 +1,6 @@
 #include "koala.h"
 #include <bits/stdc++.h>
 #pragma GCC Optimize("O3")
-#pragma GCC optimize("unroll-loops")
 using namespace std;
 
 int B[100], R[100];
@@ -58,10 +57,12 @@ void split(vector<int> v, int N, int W, int* P, int l = 1, int r = 100) {
 BEGIN ACTUAL FUNCTIONS
 */
 
-// Assign a single stone to the first cup.
-// If Koala picks that cup, it is not the smallest and some other cup has 0.
-// Otherwise, cup 0 is the smallest cup.
-// Uses 1 turn
+/*
+Assign a single stone to the first cup.
+If Koala picks that cup, it is not the smallest and some other cup has 0.
+Otherwise, cup 0 is the smallest cup.
+Uses 1 turn
+*/
 int minValue(int N, int W) {
     fill(B, B + N, 0);
     fill(R, R + N, 0);
@@ -72,8 +73,10 @@ int minValue(int N, int W) {
     return -1;
 }
 
-// Binary search for the max value.
-// Uses like 4 turns
+/*
+Binary search for the max value.
+Uses like 4 turns
+*/
 int maxValue(int N, int W) {
     vector<int> v;
     for (int i = 0; i < N; i++) v.push_back(i);
@@ -88,12 +91,14 @@ int maxValue(int N, int W) {
     return v[0];
 }
 
-// Assign positions 0 and 1 x stones until Koala treats them differently.
-// Binary search for x:
-// If both 0 and 1 get > x stones, increase x.
-// If both 0 and 1 get < x stones, decrease x.
-// Notice how x <= 9 because sum(9..17) > 100.
-// Uses like 3 turns
+/*
+Assign positions 0 and 1 x stones until Koala treats them differently.
+Binary search for x:
+If both 0 and 1 get > x stones, increase x.
+If both 0 and 1 get < x stones, decrease x.
+Notice how x <= 9 because sum(9..17) > 100.
+Uses like 3 turns
+*/
 int greaterValue(int N, int W) {
     int l = 1, r = 9;
     while (l != r) {
@@ -110,18 +115,21 @@ int greaterValue(int N, int W) {
     return (R[0] < R[1]);
 }
 
-// Subtask 4:
-// Assign indices i and j 100 stones each to check whether P[i] > P[j].
-// Use merge sort to get O(Nlog(N)) <= 700 turns
-// Subtask 5:
-// We have a recursive strategy that solves a known range [L..R] in
-// exactly L + R - 1 moves by splitting [L..R] into [L..k] and [k+1..R]
-// for some k in exactly 1 move.
-// We do this by assigning each position in the range [L..R]
-// x = min(sqrt(2 * L), M / (R - L + 1)) stones and then checking which
-// positions Koala has placed > x stones next to after the round.
-// We can easily get k this way and we always have 0 < k < L - R + 1
-// Uses exactly 99 moves
+/*
+Subtask 4:
+Assign indices i and j 100 stones each to check whether P[i] > P[j].
+Use merge sort to get O(Nlog(N)) <= 700 turns
+
+Subtask 5:
+We have a recursive strategy that solves a known range [L..R] in
+exactly L + R - 1 moves by splitting [L..R] into [L..k] and [k+1..R]
+for some k in exactly 1 move.
+We do this by assigning each position in the range [L..R]
+x = min(sqrt(2 * L), M / (R - L + 1)) stones and then checking which
+positions Koala has placed > x stones next to after the round.
+We can easily get k this way and we always have 0 < k < L - R + 1
+Uses exactly 99 turns
+*/
 void allValues(int N, int W, int *P) {
     if (W == 2 * N) {
         vector<int> v;
