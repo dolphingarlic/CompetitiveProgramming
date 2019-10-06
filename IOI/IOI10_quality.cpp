ce89e679e422649a1e06b9c4c8a1f75462138918
Andi Qu <dolphingarlic@gmail.com>
2019-10-06 12:12:35 +0200

    IOI 2010 Quality of Living

---
+++ IOI/IOI10_quality.cpp
@@ -0,0 +1,32 @@
+#include "grader.h"
+
+int lg[3001][3001], pref[3001][3001];
+
+bool check(int median, int R, int C, int H, int W, int Q[3001][3001]) {
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			if (Q[i][j] > median) lg[i + 1][j + 1] = 1;
+			else if (Q[i][j] == median) lg[i + 1][j + 1] = 0;
+			else lg[i + 1][j + 1] = -1;
+
+			pref[i + 1][j + 1] = lg[i + 1][j + 1] + pref[i + 1][j] + pref[i][j + 1] - pref[i][j];
+		}
+	}
+
+	for (int i = H; i <= R; i++) {
+		for (int j = W; j <= C; j++) {
+			if (pref[i][j] + pref[i - H][j - W] - pref[i][j - W] - pref[i - H][j] <= 0) return true;
+		}
+	}
+	return false;
+}
+
+int rectangle(int R, int C, int H, int W, int Q[3001][3001]) {
+	int l = 1, r = R * C;
+	while (l != r) {
+		int mid = (l + r) / 2;
+		if (check(mid, R, C, H, W, Q)) r = mid;
+		else l = mid + 1;
+	}
+	return l;
+}
--- POI/POI16_nim.cpp
+++ POI/POI16_nim.cpp
@@ -1,13 +1,41 @@
 #include <iostream>
+#include <queue>
+
+int l2(int a) {
+    return (a ? 1 + l2(a >> 1) : -1);
+}
+
+int leading(int a) {
+    int ans = 0;
+    for (int i = 29; ~i; i--) {
+        if (a & (1<<i)) ans++;
+        else if (ans) return ans;
+    }
+    return ans;
+}
 
 int main() {
     int n;
     std::cin >> n;
-    long long c = 1;
+    long long c = 4 * n - 1;
+    std::priority_queue<int> leadingset;
     for (int i = 0; i < n; i++) {
         int a;
         std::cin >> a;
-        while (a) a >>= 1, c += 2;
+        c += 2 * l2(a);
+        leadingset.push(leading(a));
     }
+
+    leadingset.pop();
+    while (leadingset.size()) {
+        int curr = leadingset.top();
+        leadingset.pop();
+        if (curr == 1) c -= 2;
+        else {
+            leadingset.push(curr - 1);
+            leadingset.pop();
+        }
+    }
+    
     std::cout << c;
 }
