c371c350e4f87c0e9c34c69c407f8d1dbf708914
Andi Qu <31325319+dolphingarlic@users.noreply.github.com>
2019-10-08 13:37:43 +0200

    Update BOI11_lamp.cpp

--- BOI/BOI11_lamp.cpp
+++ BOI/BOI11_lamp.cpp
@@ -1,18 +1,50 @@
 #include <bits/stdc++.h>
-#pragma GCC Optimize("O3")
 #define FOR(i, x, y) for (int i = x; i < y; i++)
-#define MOD 1000000007
-typedef long long ll;
 using namespace std;
-
-char a[500][500];
-
+ 
+int n, m;
+char orientation[2]{'\\', '/'};
+bool correct[501][501];
+int visited[501][501], dx[4]{-1, -1, 1, 1}, dy[4]{-1, 1, -1, 1};
+pair<int, int> dz[4]{{-1, -1}, {-1, 0}, {0, -1}, {0, 0}};
+ 
+bool inside(int x, int y) {
+	return (~x && x <= n && ~y && y <= m);
+}
+ 
 int main() {
-    iostream::sync_with_stdio(false);
-    cin.tie(0);
-    int n, m;
-    if ((n + m) & 1) return cout << "NO SOLUTION", 0;
-
-    FOR(i, 0, n) FOR(j, 0, m) cin >> a[i][j];
-    return 0;
+	cin >> n >> m;
+ 
+	FOR(i, 0, n) FOR(j, 0, m) {
+		char c;
+		cin >> c;
+		if (c == orientation[(i + j) & 1]) correct[i][j] = true;
+		else correct[i][j] = false;
+	}
+ 
+	deque<pair<int, pair<int, int>>> dq;
+	dq.push_back({1, {0, 0}});
+	while (dq.size()) {
+		int x, y, z;
+		tie(x, y) = dq.front().second;
+        z = dq.front().first;
+		dq.pop_front();
+        if (visited[x][y]) continue;
+        visited[x][y] = z;
+ 
+		FOR(i, 0, 4) {
+			int nx = x + dx[i], ny = y + dy[i];
+			if (inside(nx, ny) && !visited[nx][ny]) {
+				if (correct[x + dz[i].first][y + dz[i].second]) {
+					dq.push_front({z, {nx, ny}});
+				} else {
+					dq.push_back({z + 1, {nx, ny}});
+				}
+			}
+		}
+	}
+ 
+	if (visited[n][m]) cout << visited[n][m] - 1;
+	else cout << "NO SOLUTION";
+	return 0;
 }
