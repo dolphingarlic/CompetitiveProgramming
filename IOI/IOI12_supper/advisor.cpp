5e021561a8ecaa9db70ead715d6ec0573174d82b
Andi Qu <31325319+dolphingarlic@users.noreply.github.com>
2019-10-10 16:31:27 +0200

    Create advisor.cpp

---
+++ IOI/IOI12_supper/advisor.cpp
@@ -0,0 +1,49 @@
+#include "advisor.h"
+#include <bits/stdc++.h>
+#define FOR(i, x, y) for (int i = x; i < y; i++)
+using namespace std;
+ 
+set<pair<int, int>> scaffold;
+map<int, int> mp;
+queue<int> occurrences[100000];
+int swap_sequence[100000];
+bool on_scaffold[100000], active[200000];
+ 
+void ComputeAdvice(int *C, int N, int K, int M) {
+    FOR(i, 0, N) occurrences[C[i]].push(i);
+    FOR(i, 0, N) occurrences[i].push(INT_MAX);
+ 
+    FOR(i, 0, K) {
+        scaffold.insert({-occurrences[i].front(), i});
+        on_scaffold[i] = true;
+    }
+ 
+    FOR(i, 0, N) {
+        if (on_scaffold[C[i]]) {
+            swap_sequence[i] = C[i];
+            scaffold.erase({-occurrences[C[i]].front(), C[i]});
+        } else {
+            swap_sequence[i] = (*scaffold.begin()).second;
+            on_scaffold[(*scaffold.begin()).second] = false;
+            scaffold.erase(scaffold.begin());
+        }
+        occurrences[C[i]].pop();
+        scaffold.insert({-occurrences[C[i]].front(), C[i]});
+        on_scaffold[C[i]] = true;
+    }
+ 
+    FOR(i, 0, N) on_scaffold[i] = (i < K);
+ 
+    FOR(i, 0, K) mp[i] = i;
+    FOR(i, 0, N) {
+        if (on_scaffold[C[i]]) {
+            active[mp[C[i]]] = true;
+        } else {
+            on_scaffold[swap_sequence[i]] = false;
+        }
+        mp[C[i]] = i + K;
+        on_scaffold[C[i]] = true;
+    }
+ 
+    FOR(i, 0, N + K) WriteAdvice(active[i]);
+}
