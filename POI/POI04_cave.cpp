35c6733dcd1a46a45fe53349aaca3b9780964ecd
Andi Qu <dolphingarlic@gmail.com>
2019-10-28 15:29:54 +0200

    POI 2004 Cave

--- POI/POI04_cave.cpp
+++ POI/POI04_cave.cpp
@@ -1,46 +1,44 @@
 #include <bits/stdc++.h>
 #pragma GCC optimize("O3")
-#define FOR(i, x, y) for (ll i = x; i < y; i++)
+#define FOR(i, x, y) for (int i = x; i < y; i++)
 typedef long long ll;
 using namespace std;
 
-ll N;
+int N;
 
-set<ll> graph[50001];
-ll subtree[50001], ans = 0;
+vector<int> graph[50001];
+set<int> *dp[50001];
 
-////////////////////////////
-// CENTROID DECOMPOSITION //
-////////////////////////////
-void get_subtree_sizes(ll node, ll parent) {
-    subtree[node] = 1;
-    for (ll i : graph[node])
+void dfs(int node, int parent = -1) {
+    int mx_size = 0, mx_child = -1;
+    for (int i : graph[node]) {
         if (i != parent) {
-            get_subtree_sizes(i, node);
-            subtree[node] += subtree[i];
+            dfs(i, node);
+            if (dp[i]->size() > mx_size) mx_size = dp[i]->size(), mx_child = i;
         }
-}
-ll get_centroid(ll node, ll parent, ll tree_size) {
-    for (ll i : graph[node])
-        if (i != parent && subtree[i] > tree_size)
-            return get_centroid(i, node, tree_size);
-    return node;
-}
-
-/////////////////////////
-// PROCESS A COMPONENT //
-/////////////////////////
-void process(ll node, ll level = 0) {
-    get_subtree_sizes(node, 0);
-    ll centroid = get_centroid(node, 0, subtree[node] / 2);
-
-    for (ll i : graph[centroid]) {
-        graph[i].erase(centroid);
-        process(i, level + 1);
     }
-    graph[centroid].clear();
 
-    ans = max(ans, level);
+    if (mx_child == -1) dp[node] = new set<int>{0};
+    else {
+        set<int> *f = dp[mx_child];
+        int a = -1;
+        for (int i : graph[node]) {
+            if (i != parent && i != mx_child) {
+                for (int j : *dp[i]) {
+                    if (f->find(j) != f->end()) a = max(a, j);
+                    else f->insert(j);
+                }
+            }
+        }
+        int b = a + 1;
+        set<int> *ret = new set<int>(f->lower_bound(b), f->end());
+        while (ret->size() && *ret->begin() == b) {
+            b++;
+            ret->erase(ret->begin());
+        }
+        ret->insert(b);
+        dp[node] = ret;
+    }
 }
 
 int main() {
@@ -50,12 +48,11 @@ int main() {
     FOR(i, 1, N) {
         ll a, b;
         cin >> a >> b;
-        graph[a].insert(b);
-        graph[b].insert(a);
+        graph[a].push_back(b);
+        graph[b].push_back(a);
     }
 
-    process(1);
-
-    cout << ans << '\n';
+    dfs(1);
+    cout << *prev(dp[1]->end());
     return 0;
 }
