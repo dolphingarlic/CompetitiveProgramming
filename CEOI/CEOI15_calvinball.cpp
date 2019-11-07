d9d5f246f2e787102b4bc43e4edb23f8f5b499d4
Andi Qu <31325319+dolphingarlic@users.noreply.github.com>
2019-11-07 16:44:56 +0200

    Update CEOI15_calvinball.cpp

--- CEOI/CEOI15_calvinball.cpp
+++ CEOI/CEOI15_calvinball.cpp
@@ -1,20 +1,30 @@
 #include <bits/stdc++.h>
 #define FOR(i, x, y) for (int i = x; i < y; i++)
-typedef long long ll;
 using namespace std;
-
-ll dp[500][500];
-
+typedef long long ll;
+ 
+const ll MOD = 1e9 + 7;
+ 
+ll a[10001], mx[10001], dp[2][10001];
+ 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    dp[0][1] = 1;
-    FOR(i, 1, 10) {
-        FOR(j, 1, 10) {
-            FOR(k, 1, j + 2) dp[i][j] += dp[i - 1][k];
-            cout << dp[i][j] << ' ';
-        }
-        cout << '\n';
-    }
-    return 0;
+	int n;
+	cin >> n;
+	FOR(i, 0, n) {
+		dp[0][i] = 1;
+		cin >> a[i];
+		if (i) mx[i] = max(mx[i - 1], a[i - 1]);
+	}
+ 
+	ll ans = 1;
+	for (int i = n - 1; ~i; i--) {
+		ans = (ans + ((a[i] - 1) * dp[0][mx[i]]) % MOD) % MOD;
+		
+		FOR(j, 0, i)
+			dp[1][j] = (j * dp[0][j] + dp[0][j + 1]) % MOD;
+		FOR(j, 0, n + 1) dp[0][j] = dp[1][j];
+	}
+	
+	cout << ans;
+	return 0;
 }
