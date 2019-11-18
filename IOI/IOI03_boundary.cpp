528772c1a232a0c95290bf03a7b4be34d9a6e55e
Andi Qu <dolphingarlic@gmail.com>
2019-11-18 19:28:01 +0200

    IOI 2003 Seeing the Boundary

---
+++ IOI/IOI03_boundary.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#pragma GCC Optimize("unroll-loops")
+#pragma GCC target("sse4,avx2,fma,avx")
+#define FOR(i, x, y) for (int i = x; i < y; i++)
+#define x first
+#define y second
+typedef long long ll;
+using namespace std;
+
+const double PI = 4 * atan(1);
+
+struct Event {
+    int type, id;
+    pair<int, int> loc;
+};
+
+pair<int, int> origin, polygon[20];
+bool cmp(pair<int, int> a, pair<int, int> b) {
+    return (a.x - origin.x) * (b.y - origin.y) < (a.y - origin.y) * (b.x - origin.x);
+}
+bool operator<(Event a, Event b) {
+    double angle_a = atan2(a.loc.x - origin.x, a.loc.y - origin.y);
+    double angle_b = atan2(b.loc.x - origin.x, b.loc.y - origin.y);
+    if (angle_a < 0) angle_a += 2 * PI;
+    if (angle_b < 0) angle_b += 2 * PI;
+
+    if (angle_a == angle_b) return a.type > b.type;
+    return angle_a < angle_b;
+}
+
+vector<Event> events;
+bool before[40000];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n, r;
+    cin >> n >> r >> origin.x >> origin.y;
+
+    FOR(i, 0, r) {
+        int m;
+        cin >> m;
+        FOR(j, 0, m) cin >> polygon[j].x >> polygon[j].y;
+        sort(polygon, polygon + m, cmp);
+        events.push_back({1, i, polygon[0]});
+        events.push_back({-1, i, polygon[m - 1]});
+    }
+
+    FOR(i, 0, n) {
+        events.push_back({0, 0, {0, i}});
+        events.push_back({0, 0, {i, n}});
+        events.push_back({0, 0, {n, n - i}});
+        events.push_back({0, 0, {n - i, 0}});
+    }
+
+    sort(events.begin(), events.end());
+    int active = 0;
+    for (Event i : events) {
+        if (i.type == 1) before[i.id] = true;
+        else if (i.type == -1 && !before[i.id]) active++;
+    }
+
+    int ans = 0;
+    for (Event i : events) {
+        if (i.type == 0) {
+            if (!active) ans++;
+        } else if (i.type == 1) active++;
+        else active--;
+    }
+
+    cout << ans;
+    return 0;
+}
