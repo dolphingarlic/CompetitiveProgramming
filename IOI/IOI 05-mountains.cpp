57668d5842a32e14ef4009e936892f89f5658542
Andi Qu <dolphingarlic@gmail.com>
2019-12-16 07:30:06 +0200

    Renamed IOI problems

---
+++ IOI/IOI 05-mountains.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define FOR(i, x, y) for (int i = x; i < y; i++)
+typedef long long ll;
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    return 0;
+}
