66f9707a017e9c88ac34e80361812313937b2826
Andi Qu <dolphingarlic@gmail.com>
2020-04-14 20:24:24 +0200

    Balkan 2011 Day 2

--- Balkan/Balkan 11-timeismoney.cpp
+++ Balkan/Balkan 11-timeismoney.cpp
@@ -19,12 +19,10 @@ struct Line {
     bool operator<(Line B) { return w < B.w; }
 } lines[10000];
 
-struct Point {
-    ll x, y;
-};
+struct Point { ll x, y; };
 
-bool collinear(Point A, Point B, Point C){
-	return (B.x - A.x) * (C.y - A.y) == (B.y - A.y) * (C.x - A.x);
+bool collinear(Point A, Point B, Point C) {
+    return (B.x - A.x) * (C.y - A.y) == (B.y - A.y) * (C.x - A.x);
 }
 
 int cmp[200];
@@ -74,7 +72,7 @@ int main() {
     cin.tie(0);
     cin >> n >> m;
     FOR(i, 0, m) cin >> lines[i].u >> lines[i].v >> lines[i].t >> lines[i].c;
-    
+
     Point A = mst(0, 1), B = mst(1, 0);
     recurse(A, B);
 
