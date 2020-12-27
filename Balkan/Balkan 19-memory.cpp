33711ddb808fc1c12ee4bc84deb36af4e4dc94ce
Andi Qu <dolphingarlic@gmail.com>
2020-12-28 00:43:13 +0200

    Actually solved Memory this time

--- Balkan/Balkan 19-memory.cpp
+++ Balkan/Balkan 19-memory.cpp
@@ -1,6 +1,5 @@
 /*
 Balkan 2019 Clear the Memory
-- Binary search for the answer
 - When we check a candidate pref, we do a reverse BFS
 - Initially, we only process the programs with no 1s in [1, pref]
 - We need a data structure that supports the following two operations:
@@ -15,7 +14,8 @@ Balkan 2019 Clear the Memory
     - Finally, we can decrement a counter for each program of each range
     - When the counter reaches 0, we push the program into the queue
 - pref works iff each bit in [1, pref] has been set to 0
-- Complexity: O(M log^2 M)
+- To avoid binsearch, we do magic greedy stuff that I'm too tired to explain
+- Complexity: O(M log M)
 */
 
 #include <bits/stdc++.h>
@@ -27,6 +27,7 @@ int n, m, t;
 vector<pair<int, int>> has_0[500001], has_1[500001];
 tuple<int, int, int> segtree[2000001];
 int cnt_1[500001], ptr[500001];
+queue<int> q;
 
 void update(int pos, tuple<int, int, int> val, int node = 1, int l = 1, int r = m) {
     if (l == r) segtree[node] = val;
@@ -45,53 +46,18 @@ tuple<int, int, int> query(int a, int b, int node = 1, int l = 1, int r = m) {
     return min(query(a, b, node * 2, l, mid), query(a, b, node * 2 + 1, mid + 1, r));
 }
 
-bool check(int pref) {
-    if (pref > m) return false;
-
-    memset(cnt_1, 0, sizeof cnt_1);
-    set<int> unset;
-    for (int i = 1; i <= pref; i++) {
-        ptr[i] = 0;
-        unset.insert(i);
-        if (has_1[i].size() && has_1[i][0].first <= pref) {
-            ptr[i]++;
-            update(i, make_tuple(has_1[i][0].first, i, has_1[i][0].second));
-            for (pair<int, int> j : has_1[i]) {
-                if (j.first > pref) break;
-                cnt_1[j.second]++;
-            }
-        } else update(i, MN);
+void queryupdate(int a, int b) {
+    int r, l, p;
+    tie(r, l, p) = query(a, b);
+    while (r <= b) {
+        cnt_1[p]--;
+        if (!cnt_1[p]) q.push(p);
+        if (ptr[l] < has_1[l].size()) {
+            update(l, make_tuple(has_1[l][ptr[l]].first, l, has_1[l][ptr[l]].second));
+            ptr[l]++;
+        } else update(l, MN);
+        tie(r, l, p) = query(a, b);
     }
-    unset.insert(0), unset.insert(pref + 1);
-
-    queue<int> q;
-    for (int i = 1; i <= n; i++) if (!cnt_1[i]) q.push(i);
-    while (q.size()) {
-        int curr = q.front();
-        q.pop();
-        for (pair<int, int> i : has_0[curr]) if (i.first <= pref) {
-            set<int>::iterator lb = unset.lower_bound(i.first);
-            while (*lb <= min(pref, i.second)) {
-                int a = *prev(lb) + 1, b = *next(lb) - 1;
-
-                int r, l, p;
-                tie(r, l, p) = query(a, b);
-                while (r <= pref) {
-                    cnt_1[p]--;
-                    if (!cnt_1[p]) q.push(p);
-
-                    if (ptr[l] < has_1[l].size() && has_1[l][ptr[l]].first <= pref) {
-                        update(l, make_tuple(has_1[l][ptr[l]].first, l, has_1[l][ptr[l]].second));
-                        ptr[l]++;
-                    } else update(l, MN);
-                    tie(r, l, p) = query(a, b);
-                }
-                unset.erase(lb);
-                lb = unset.lower_bound(i.first);
-            }
-        }
-    }
-    return unset.size() == 2;
 }
 
 int main() {
@@ -100,13 +66,45 @@ int main() {
     while (t--) {
         int p, l, r, v;
         cin >> p >> l >> r >> v;
-        if (v) has_1[l].push_back({r, p});
-        else has_0[p].push_back({l, r});
+        if (v) {
+            has_1[l].push_back({r, p});
+            cnt_1[p]++;
+        } else has_0[p].push_back({l, r});
     }
-    for (int i = 1; i <= m; i++) sort(has_1[i].begin(), has_1[i].end());
+    
+    set<int> unset; unset.insert(0), unset.insert(m + 1);
+    for (int i = 1; i <= m; i++) {
+        sort(has_1[i].begin(), has_1[i].end());
+        unset.insert(i);
+        if (has_1[i].size()) {
+            ptr[i] = 1;
+            update(i, make_tuple(has_1[i][0].first, i, has_1[i][0].second));
+        } else update(i, MN);
+    }
 
-    int ans = 0;
-    for (int i = 1 << 18; i; i >>= 1) ans += check(ans + i) * i;
-    cout << ans;
+    for (int i = 1; i <= n; i++) if (!cnt_1[i]) q.push(i);
+    for (int pref = m; ~pref;) {
+        while (q.size()) {
+            int curr = q.front();
+            q.pop();
+            for (pair<int, int> i : has_0[curr]) {
+                set<int>::iterator lb = unset.lower_bound(i.first);
+                while (*lb <= min(pref, i.second)) {
+                    int a = *prev(lb) + 1, b = *next(lb) - 1;
+                    queryupdate(a, b);
+                    unset.erase(lb);
+                    lb = unset.lower_bound(i.first);
+                }
+            }
+        }
+
+        if (!*next(unset.rbegin())) return cout << pref, 0;
+
+        auto lb = next(unset.rbegin());
+        int a = *next(lb) + 1, b = m;
+        queryupdate(a, b);
+        pref = *lb - 1;
+        unset.erase(*lb);
+    }
     return 0;
 }
