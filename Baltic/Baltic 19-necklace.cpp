29d94a0874d2b6a35774a5f5e883e5cd76518799
Andi Qu <dolphingarlic@gmail.com>
2021-02-01 17:46:19 +0200

    BOI 19 Necklace

--- Baltic/Baltic 19-necklace.cpp
+++ Baltic/Baltic 19-necklace.cpp
@@ -1,64 +1,57 @@
 /*
 Baltic 2019 Necklace
-- Firstly, assume WLOG that we can't flip the necklace.
-    - If we do flip it in the optimal solution, just reverse B and run the
-      same solution again
-- Notice how we'll have A = ...XY... and B = ...YX... where X and Y are substrings
-- lcs[i][j] = Longest common suffix of A[:i] and B[:j]
-            = lcs[i - 1][j - 1] + 1 if A[i] == B[j] else 0
-- dp1[i][j] = Longest suffix of A[:i] that's also a prefix of B[j:]
-    - First, dp1[i][j - lcs[i][j]] = max(dp1[i][j - lcs[i][j]], lcs[i][j])
-    - Next, dp1[i][j] = max(dp1[i][j], dp1[i][j - 1] - 1)
-- dp2[i][j] = Longest prefix of A[i:] that's also a suffix of B[:j]
-    - We compute this similarly
-- To get this down to O(N) memory, notice how dp[i] only depends on lcs[i]
-  and lcs[i] only depends on lcs[i - 1]
+- Assume for simplicity that we can't flip over the necklace
+- For each i from 0 to N...
+    - Let S1 = reversed(S[0 .. i - 1]) and S2 = S[i .. N - 1]
+    - Let T1 = T and T2 = reversed(T)
+    - Construct the prefix functions of the strings (S1 + "#" + T1) and (S2 + "#" + T2)
+    - Using these prefix functions, we can find the longest necklaces we can make if we
+      rotate it so that bead S[i] is at the front
+- Afterward, reverse T and solve the problem again
 - Complexity: O(N^2) with O(N) memory
 */
 
 #include <bits/stdc++.h>
-#define FOR(i, x, y) for (int i = x; i < y; i++)
 typedef long long ll;
 using namespace std;
 
-int lcs[3001][3001], dp1[3001][3001], dp2[3001][3001];
-int mx_len, best_i, best_j;
+int p1[6002], p2[6002];
 
-void solve(string A, string B, bool reversed) {
-    memset(lcs, 0, sizeof lcs);
-    memset(dp1, 0, sizeof dp1);
-    memset(dp2, 0, sizeof dp2);
-    FOR(i, 1, A.size() - 1) {
-        FOR(j, 1, B.size() - 1) {
-            lcs[i][j] = (A[i] == B[j] ? lcs[i - 1][j - 1] + 1 : 0);
-            dp1[i][j - lcs[i][j]] = max(dp1[i][j - lcs[i][j]], lcs[i][j]);
-            dp2[i - lcs[i][j]][j] = max(dp2[i - lcs[i][j]][j], lcs[i][j]);
-        }
-    }
-    FOR(i, 1, A.size() - 1) {
-        FOR(j, 1, B.size() - 1) {
-            dp1[i][j] = max(dp1[i][j], dp1[i][j - 1] - 1);
-            dp2[i][j] = max(dp2[i][j], dp2[i - 1][j] - 1);
-
-            if (dp1[i][j] + dp2[i][j] > mx_len) {
-                mx_len = dp1[i][j] + dp2[i][j];
-                best_i = i - dp1[i][j];
-                if (!reversed) best_j = j - dp2[i][j];
-                else best_j = B.size() - j - dp1[i][j] - 2;
-            }
-        }
+void fill_p(string s, int *p) {
+    for (int i = 1; i < s.size(); i++) {
+        int j = p[i - 1];
+        while (j && s[i] != s[j]) j = p[j - 1];
+        if (s[i] == s[j]) j++;
+        p[i] = j;
     }
 }
 
+tuple<int, int, int> solve(string s, string t, bool rev) {
+    int n = s.size(), m = t.size();
+    tuple<int, int, int> ans = {0, 0, 0};
+    for (int i = 0; i < n; i++) {
+        string s1 = s.substr(0, i), s2 = s.substr(i, n - i);
+        reverse(s1.begin(), s1.end());
+        string t1 = t, t2 = t;
+        reverse(t2.begin(), t2.end());
+        fill_p(s1 + "#" + t1, p1), fill_p(s2 + "#" + t2, p2);
+        for (int j = 1; j <= m; j++)
+            ans = max(ans, {
+                p1[i + j] + p2[n + m - i - j],
+                i - p1[i + j],
+                rev ? m - j - p2[n + m - i - j] : j - p1[i + j]
+            });
+    }
+    return ans;
+}
+
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    string A, B;
-    cin >> A >> B;
-    A = "." + A + ".", B = "." + B + ".";
-    solve(A, B, 0);
-    reverse(B.begin(), B.end());
-    solve(A, B, 1);
-    cout << mx_len << '\n' << best_i << ' ' << best_j;
+    cin.tie(0)->sync_with_stdio(0);
+    string s, t;
+    cin >> s >> t;
+    tuple<int, int, int> ans = solve(s, t, false);
+    reverse(t.begin(), t.end());
+    ans = max(ans, solve(s, t, true));
+    cout << get<0>(ans) << '\n' << get<1>(ans) << ' ' << get<2>(ans);
     return 0;
 }
